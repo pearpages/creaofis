c0bf2be625a24f67896492928113e139ee74cc67
pearpages <pere@soms.cat>
2019-02-22 13:12:24 +0100

    fix: iframe

--- projects/cui-ui/src/lib/cui-snake/cui-snake.component.ts
+++ projects/cui-ui/src/lib/cui-snake/cui-snake.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'cui-snake',
   template: `
-    <iframe src="https://pearpages.com/games/snake" width="600px" height="600px" frameborder="0"></iframe>
+    <iframe src="https://pearpages.com/games/snake/" width="600px" height="600px" frameborder="0"></iframe>
   `
 })
 export class CuiSnakeComponent implements OnInit {
