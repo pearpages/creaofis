6eb8e446499547475be8d14b8c660257759533e1
pearpages <pere@soms.cat>
2019-02-23 19:42:15 +0100

    refactor: remove unused files

--- projects/bss-business/src/public_api.ts
+++ projects/bss-business/src/public_api.ts
@@ -2,7 +2,5 @@
  * Public API Surface of bss-business
  */
 
-export * from './lib/bss-business.service';
-export * from './lib/bss-business.component';
 export * from './lib/bss-business.module';
 export * from './lib/bss-business.models';
